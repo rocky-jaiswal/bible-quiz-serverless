ac2ce37a6c2966460d1749d9e619da7132bccafb
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2021-08-10 12:58:14 +0200

    update model

--- src/models/question.ts
+++ src/models/question.ts
@@ -1,5 +1,16 @@
-import { hashKey, table } from '@aws/dynamodb-data-mapper-annotations'
-import { Field, ObjectType } from 'type-graphql'
+import { attribute, hashKey, table } from '@aws/dynamodb-data-mapper-annotations'
+import { Field, ObjectType, Int } from 'type-graphql'
+
+@ObjectType()
+class Option {
+  @Field(_type => Int)
+  @attribute()
+  id?: number
+
+  @Field({ nullable: false })
+  @attribute()
+  text?: string
+}
 
 @ObjectType()
 @table('bible_quiz_questions')
@@ -7,6 +18,18 @@ class Question {
   @Field({ nullable: false })
   @hashKey()
   public id?: string
+
+  @Field({ nullable: false })
+  @attribute()
+  public question?: string
+
+  @Field({ nullable: false })
+  @attribute()
+  public type?: string
+
+  @Field(_type => [Option])
+  @attribute()
+  public options?: Array<Option>
 }
 
 export default Question
